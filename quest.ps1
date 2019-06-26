d290cd699e017e24fb5f197743ce9339282b7202
fuat fucucuoglu <40765180+ffucucuoglu@users.noreply.github.com>
2019-06-26 15:28:09 +0300

    Add files via upload

---
+++ quest.ps1
@@ -0,0 +1,8 @@
+﻿
+
+
+if((Get-ScheduledTask | Where TaskName -eq taskschedulerdeneme ).State -eq "Ready"){
+    powershell C:\Users\fuatf\Desktop\Untitled2.ps1
+}else{
+    schtasks.exe /create /tn pwrshldeneme /sc daily /tr "powershell -WindowStyle Hidden .\questek.ps1" /st 12:50
+}
---
+++ questek.ps1
@@ -0,0 +1,15 @@
+﻿
+$today=(Get-Date).Hour.ToString()+"_"+(Get-Date).Day.ToString()+"_"+(Get-Date).Month.ToString()+"_"+(Get-Date).Year.ToString()+".txt"
+$ystrdy=Get-Date
+$ystrdy=$ystrdy.AddDays(-1)
+
+$discLetters=(Get-Partition|Where-Object -FilterScript {$_.Type -Eq "Basic"}).DriveLetter
+
+for ($i = 0; $i -le ($discLetters.length - 1); $i += 1) {
+    $discLetters[$i]=$discLetters[$i]+":\"
+    Get-ChildItem $discLetters[$i] -ErrorAction SilentlyContinue -Recurse|ForEach-Object{#-force
+        if($_.LastWriteTime -ge $ystrdy){
+            (echo $_.LastWriteTime $_.FullName )>>C:\Users\fuatf\Desktop\$today
+        }
+    }
+}
