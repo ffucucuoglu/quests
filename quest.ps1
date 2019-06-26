3aa13b3cdd8310892c8099e3b391a5c7062a45fe
fuat fucucuoglu <40765180+ffucucuoglu@users.noreply.github.com>
2019-06-26 15:29:05 +0300

    Delete quest.ps1

--- quest.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-
-
-if((Get-ScheduledTask | Where TaskName -eq taskschedulerdeneme ).State -eq "Ready"){
-    powershell C:\Users\fuatf\Desktop\Untitled2.ps1
-}else{
-    schtasks.exe /create /tn pwrshldeneme /sc daily /tr "powershell -WindowStyle Hidden .\questek.ps1" /st 12:50
-}
