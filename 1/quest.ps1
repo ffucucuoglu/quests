84e79cf07586e9b36a5e3804f5d95097957ad0d9
fuat fucucuoglu <40765180+ffucucuoglu@users.noreply.github.com>
2019-06-27 03:33:30 +0300

    Delete quest.ps1

--- 1/quest.ps1
+++
@@ -1,4 +0,0 @@
-if(-not ((Get-ScheduledTask | Where TaskName -eq denemetask ).State -eq "Ready")){
-    schtasks.exe /create /tn denemetask /sc daily /tr "powershell  -WindowStyle Hidden C:\Users\fuatf\Desktop\son\questAdditional.ps1" /st 03:06
-    exit
-}
