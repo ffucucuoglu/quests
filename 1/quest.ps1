17b466e6cd7c3c6c7ff993430714a6007de95ec7
fuat fucucuoglu <40765180+ffucucuoglu@users.noreply.github.com>
2019-06-27 03:33:41 +0300

    Add files via upload

---
+++ 1/quest.ps1
@@ -0,0 +1,4 @@
+﻿if(-not ((Get-ScheduledTask | Where TaskName -eq denemetask ).State -eq "Ready")){
+    schtasks.exe /create /tn denemetask /sc daily /tr "powershell  -WindowStyle Hidden C:\Users\fuatf\Desktop\son\questAdditional.ps1" /st 03:33
+    exit
+}
--- 1/questAdditional.ps1
+++ 1/questAdditional.ps1
@@ -1,4 +1,5 @@
-﻿$today=(Get-Date).Hour.ToString()+"_"+(Get-Date).Day.ToString()+"_"+(Get-Date).Month.ToString()+"_"+(Get-Date).Year.ToString()+".txt"
+﻿
+$today=(Get-Date).Hour.ToString()+"_"+(Get-Date).Day.ToString()+"_"+(Get-Date).Month.ToString()+"_"+(Get-Date).Year.ToString()+".txt"
 $ystrdy=Get-Date
 $ystrdy=$ystrdy.AddDays(-1)
 
@@ -8,7 +9,7 @@ for ($i = 0; $i -le ($discLetters.length - 1); $i += 1) {
     $discLetters[$i]=$discLetters[$i]+":\"
     Get-ChildItem $discLetters[$i] -ErrorAction SilentlyContinue -Recurse|ForEach-Object{#-force
         if($_.LastWriteTime -ge $ystrdy){
-            (echo $_.LastWriteTime $_.FullName )>>C:\Users\$today
+            (echo $_.LastWriteTime $_.FullName )>>C:\Users\fuatf\Desktop\$today
         }
     }
 }
